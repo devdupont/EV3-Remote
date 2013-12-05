a066da5249d383d3ac27b5e45f9bf49a7e836bc6
Michael duPont <flyinactor91@gmail.com>
2013-12-05 15:59:52 -0500

    Screen as Class, added some error handling

--- ScreenSocket.py
+++ ScreenSocket.py
@@ -1,80 +1,90 @@
 #!/usr/bin/env python
 
-##--Zachary Trette
-##-- accepts commands for screen responses
+##-- Zachary Trette & Michael duPont
+##-- Accepts commands for screen responses
 ##-- EV3- Remote - https://github.com/flyinactor91/EV3-Remote
 
-## 2013-12-1
+## 2013-12-05
 
+import os , pygame
 from socket import *
-import sys, os
-import pygame
 from pygame.locals import *
 
-def setup():
-    pygame.init()
-    w = 640
-    h = 480
-    size=(w,h)
-    screen = pygame.display.set_mode(size,HWSURFACE|DOUBLEBUF|RESIZABLE)
-    return screen, size
-
-
-def runCue(SorI, strOrImage):
-    if SorI == "I":
-        im = pygame.image.load(strOrImage)
-        scrn.blit(pygame.transform.scale(im,size),(0,0))
-	pygame.display.flip()	
-    elif SorI == "T":
-	basicfont = pygame.font.SysFont(None, 48)
-	text = basicfont.render(strOrImage, True, (255, 0, 0), (0, 0, 0))
-	textrect = text.get_rect()
-	textrect.centerx = scrn.get_rect().centerx
-	textrect.centery = scrn.get_rect().centery
-	scrn.fill((0,0,0))
-	scrn.blit(text, textrect)
-	pygame.display.flip()
-    elif SorI == "C":
-	basicfont = pygame.font.SysFont(None, 48)
-	text = basicfont.render("", True, (0, 0, 0), (0, 0, 0))
-	textrect = text.get_rect()
-	textrect.centerx = scrn.get_rect().centerx
-	textrect.centery = scrn.get_rect().centery
-	scrn.fill((0,0,0))
-	scrn.blit(text, textrect)
-	pygame.display.flip()
-
-
-TCP_PORT = 5678
+port = 5678
 defaultTimeout = 5
-if len(sys.argv) == 2:
-	TCP_IP = sys.argv[1]
 
-BUFFER_SIZE = 1024
-screenSocket = socket(AF_INET, SOCK_STREAM)
-screenSocket.bind(('' , TCP_PORT))
-screenSocket.listen(1)
-dne = False
-scrn, size = setup()
-while not dne:
-	connectionSocket , addr = screenSocket.accept()
-	connectionSocket.settimeout(defaultTimeout)
-	msg = connectionSocket.recv(BUFFER_SIZE)
-        msg = msg.strip()
-	if msg == 'QUIT':
-		print "DONE"
-		dne = True
-	else:	
-		t = msg[0]
-		s = msg[1:].strip()
-		runCue(t,s)
+class screen:
+	#Init window
+	def __init__(self , ht , wt):
+		pygame.init()
+		self.size = (ht , wt)
+		self.win = screen = pygame.display.set_mode(self.size,HWSURFACE|DOUBLEBUF|RESIZABLE)
 	
-	#connectionSocket.send()
-	connectionSocket.close()
+	#Display text in window
+	def showText(self , txt):
+		basicfont = pygame.font.SysFont(None, 48)
+		text = basicfont.render(txt , True , (255,0,0), (0,0,0))
+		textrect = text.get_rect()
+		textrect.centerx = self.win.get_rect().centerx
+		textrect.centery = self.win.get_rect().centery
+		self.win.fill((0,0,0))
+		self.win.blit(text , textrect)
+		pygame.display.flip()
+	
+	#Display image in window
+	def showImg(self , fName):
+		if os.path.isfile(fName):
+			img = pygame.image.load(fName)
+			self.win.blit(pygame.transform.scale(img , self.size) , (0,0))
+			pygame.display.flip()
+		else: print 'File not found: ' + fName
+	
+	#Clear text or image from screen
+	def clearWin(self):
+		basicfont = pygame.font.SysFont(None , 48)
+		text = basicfont.render("" , True , (0,0,0) , (0,0,0))
+		textrect = text.get_rect()
+		textrect.centerx = self.win.get_rect().centerx
+		textrect.centery = self.win.get_rect().centery
+		self.win.fill((0,0,0))
+		self.win.blit(text , textrect)
+		pygame.display.flip()
 
-screenSocket.close()
+
+def main():
+	#Init socket
+	screenSocket = socket(AF_INET, SOCK_STREAM)
+	screenSocket.bind(('' , port))
+	screenSocket.listen(1)
+	
+	#Create screen
+	disp = screen(640 , 480)
+	quitFlag = False
+	while not quitFlag:
+		
+		#Recieve connection / command
+		connectionSocket , addr = screenSocket.accept()
+		connectionSocket.settimeout(defaultTimeout)
+		msg = connectionSocket.recv(1024).strip()
+		
+		#Command indent
+		#Text
+		if msg[0] == 'T': disp.showText(msg[1:].strip())
+		#Image
+		elif msg[0] == 'I': disp.showImg(msg[1:].strip())
+		#Clear
+		elif msg[0] == 'C': disp.clearWin()
+		#Quit
+		elif msg == 'QUIT': quitFlag = True
+		#Catch
+		else: print 'Not a valid command'
+		
+		#Close connection
+		connectionSocket.close()
+	
+	#Close socket
+	screenSocket.close()
 
 
-
-
-    
+if __name__ == '__main__':
+	main()
