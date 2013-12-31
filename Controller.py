e823ad93e1c0969fe5c33a57dc9a94be8049220a
Michael duPont <flyinactor91@gmail.com>
2013-12-31 01:30:08 -0500

    Rewrote Controller, reformatted Screen

--- Controller.py
+++ Controller.py
@@ -4,130 +4,119 @@
 ##--Control hub for multiple robots/clients
 ##--EV3-Remote - https://github.com/flyinactor91/EV3-Remote
 
-##--2013-12-25
+##--2013-12-31
 ##--Ex Cue: '0 : F 500;LED 4 | 2 : L 400;BEEP 4'
 
 import socket , os , sys
 
 port = 5678
 defaultTimeout = 5
-cueNum = 0
-lastCue = -1
-cueList , host , quitFlag = [] , [] , []
 
-#Creates socket and send/recieve a single string of data
-def sendDATA(recvr , command):
-	global cueNum
+class host:
+	def __init__(self , IP):
+		self.IP = IP
+		self.isOn = True
+	def turnOff(self):
+		self.isOn = False
+
+def sendDATA(IP , command):
+	"""Creates socket and send/recieve a single string of data
+	Returns the recieved string or error message"""
 	try:
-		#Create socket
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create socket
 		sock.settimeout(defaultTimeout)
-		sock.connect((host[recvr], port))
-		#Send command
-		sock.send(command+'\n')
-		print "Cue "+str(cueNum)+' to '+str(recvr)+': '+command
-		#Recv confirmation
-		ret = sock.recv(1024)
-		if ret.find("\n") != -1: print ret[:ret.find("\n")]
-		else: print ret
-		#Close socket
-		sock.close()
+		sock.connect((IP, port))
+		sock.send(command+'\n') #Send command
+		ret = sock.recv(1024) #Recv confirmation
+		sock.close() #Close socket
+		if ret.find('\n') != -1: return ret[:ret.find('\n')]
+		return ret
 	#Catch communication errors
 	except Exception , e:
-		print 'Error: ' + str(e)[str(e).find(']')+1:]
+		return 'Connection error\nError: ' + str(e)[str(e).find(']')+1:]
 
-
-#Send stand-alone command to selected recvr. Returns None
-#If cue or command contains "QUIT", sets recvr's quitFlag to False
-#Ex. sendCommand('1 : F 1000 ; LED 3')
-def sendCommand(cmd):
+def sendCommand(cmd , hosts , cueNum):
+	"""Send stand-alone command to selected recvr. If cue or command contains "QUIT", call host.turnOff(). Returns hosts
+	Ex. sendCommand('1 : F 1000 ; LED 3' , hosts , cueNum)"""
 	cmd = cmd.strip().split('|')
 	for i in range(len(cmd)):
 		try:
-			recvr = int(cmd[i].split(':')[0].strip())
+			hostNum = int(cmd[i].split(':')[0].strip())
 			command = cmd[i].split(':')[1].strip()
-			sendDATA(recvr , command)
-			#If command contains "QUIT", set recvr's quitFlag to False
-			if command.find('QUIT') != -1: quitFlag[recvr] = False
+			if cueNum == -1: print 'Command'+' to '+str(hostNum)+': '+command
+			else: print 'Cue '+str(cueNum)+' to '+str(hostNum)+': '+command
+			print sendDATA(hosts[hostNum].IP , command)
+			if command.find('QUIT') != -1: hosts[hostNum].turnOff()
 		except Exception , e:
-			print 'Error: ' + str(e)[str(e).find(']')+1:]
+			print 'Command text error\nError: ' + str(e)[str(e).find(']')+1:]
+	return hosts
 
+def anyOn(hosts):
+	"""Returns true if any hosts are on"""
+	for obj in hosts:
+		if obj.isOn: return True
+	return False
 
-#Returns True if any value in list is True. Else False
-def checkTrue(lst):
-	ret = False
-	for itm in lst:
-		if itm == True: ret = True
-	return ret
-
-
-def main():
-	global cueNum , lastCue , cueList , host , quitFlag
-	loadShow = False
-	
+def setup():
+	"""Returns hosts, cueList, and cueNum vars. Value contents based on how program is called
+	Ex.  ./Controller.py cues.txt  or  ./Controller.py 192.168.42.1 192.168.42.2"""
+	hosts , cueList = [] , []
+	cueNum = -1
 	#If loading show from a file--##
 	#Ex. ./Controller.py cues.txt
 	if len(sys.argv) == 2 and sys.argv[1].find('.txt') != -1:
-		fin = open(sys.argv[1] , 'rb')
-		cueList = fin.readlines()
-		#Iterate through file and remove empty and comment lines
-		remv = []
-		for i in range(len(cueList)):
-			if cueList[i] == '\n' or cueList[i] == '' or cueList[i][0] == '#': remv.append(i)
-		remv.reverse()
-		for i in range(len(remv)):
-			cueList.pop(remv[i])
-		#Make list of IPs and associated quitFlags, then remove first (IP) line
-		for IP in cueList[0].split(','):
-			host.append(IP.strip())
-			quitFlag.append(True)
-		cueList.pop(0)
-		#Remove '\n' from the end of each line
-		for line in range(len(cueList)): cueList[line] = cueList[line].strip('\n')
-		loadShow = True
+		try:
+			fin = open(sys.argv[1] , 'rb')
+			rawLines = fin.readlines()
+			#Add non-empty and non-comment lines to cueList
+			for line in rawLines:
+				if line != '\n' and line != '' and line[0] != '#': cueList.append(line[:line.find('\n')])
+			#Make list of IPs and associated quitFlags, then remove first (IP) line
+			for IP in cueList[0].split(','): hosts.append(host(IP.strip()))
+			cueList.pop(0)
+			cueNum = 0
+		except Exception , e:
+			sys.exit('Error loading show. Check your txt file for errors.\nError: ' + str(e)[str(e).find(']')+1:])
 	#If loading IPs (not loading show)--##
 	#Ex. ./Controller.py 192.168.42.1 192.168.42.2
 	elif len(sys.argv) > 1:
-		for i in range(1 , len(sys.argv)):
-			host.append(sys.argv[i].strip())
-			quitFlag.append(True)
+		for i in range(1 , len(sys.argv)): hosts.append(host(sys.argv[i].strip()))
 		cueList = ['']
-		print host , quitFlag
 	#Exit if incorrectly called
 	else: sys.exit("Usage:\n./Controller.py (file.txt)\t\t- loading show from a file\n./Controller.py (IP address, ...)\t- loading IP(s) from terminal")
+	return hosts , cueList , cueNum
+	
+def main():
+	hosts , cueList , cueNum = setup() #If cueNum = -1, the program knows a show has not been loaded
+	lastCue = -1
 	
 	#Init terminal
 	os.system('clear')
 	print "Accepts blank (ENTER), cue number, or stand-alone command.\nExample command:  0 : F 500 ; LED 3\nMultiple robots:  0 : LED 1 | 1 : LED 2"
 	
-	while checkTrue(quitFlag):		#Exits once all clients have QUIT
-		
+	while anyOn(hosts): #Exits once all clients have QUIT
 		#Get command
-		if loadShow:
+		if cueNum != -1:
 			if cueNum != len(cueList): print '\nNext cue #' + str(cueNum) + '\t' + str(cueList[cueNum])
 			cue = raw_input('Last: '+str(lastCue)+'  Next: ')
 		else: cue = raw_input('\nCommand: ')
-		
 		#If continue or cue entered
 		if (cue == '') or (cue.isdigit()):
-			#Only do cue lookup if a show has been loaded
-			if loadShow:
-				if cue.isdigit(): cueNum = int(cue)			#Set cueNum if new cue is entered
-				if not (-1 < cueNum < len(cueList)):		#Check cueNum is within bounds. Else reset to previous cueNum
+			if cueNum != -1: #Only do cue lookup if a show has been loaded
+				if cue.isdigit(): cueNum = int(cue) #Set cueNum if new cue is entered
+				if not (-1 < cueNum < len(cueList)): #Check cueNum is within bounds. Else reset to previous cueNum
 					print 'Cue is out of bounds. Reverting to last cue'
 					cueNum = lastCue+1
 				else:
-					sendCommand(cueList[cueNum])
+					hosts = sendCommand(cueList[cueNum] , hosts , cueNum)
 					#Update queNums
 					lastCue = cueNum
 					cueNum += 1
 			else: print 'No show has been loaded'
-		
 		#Else stand-alone command is entered
 		else:
-			sendCommand(cue)
+			hosts = sendCommand(cue , hosts , cueNum)
 	print 'End Program'
-
 
 if __name__ == '__main__':
 	main()
--- etc/Screen.py
+++ etc/Screen.py
@@ -4,7 +4,7 @@
 ##-- Accepts commands for screen responses
 ##-- EV3- Remote - https://github.com/flyinactor91/EV3-Remote
 
-## 2013-12-25
+## 2013-12-31
 
 import os , pygame
 from socket import *
@@ -16,127 +16,116 @@ defaultTimeout = 5
 colorLib = { 'B' : (0,0,0) , 'R' : (255,0,0) , 'G' : (0,255,0) , 'O' : (255,115,0) }
 
 class screen:
-        #Init window
-        def __init__(self , ht , wt):
-                pygame.init()
-                self.size = (ht , wt)
-                self.win = screen = pygame.display.set_mode(self.size,HWSURFACE|DOUBLEBUF|RESIZABLE)
-        
-        #Clear text or image from screen
-        def clearWin(self):
-                basicfont = pygame.font.SysFont(None , 48)
-                text = basicfont.render("" , True , colorLib['B'] , (0,0,0))
-                textrect = text.get_rect()
-                textrect.centerx = self.win.get_rect().centerx
-                textrect.centery = self.win.get_rect().centery
-                self.win.fill(colorLib['B'])
-                self.win.blit(text , textrect)
-                pygame.display.flip()
-        
-        #Display text in window
-        def showText(self , txt,c):
-                resize = 1 #Alter this value to adjust text width relative to window width
-                self.clearWin()
-                self.win.fill(colorLib['B'])
-                basicfont = pygame.font.SysFont(None, 128)
-                sw, sh = self.size
-                w, h = basicfont.size(txt) #gets size of font
-                lines = self.wrapline(txt,basicfont,self.size[1]*resize) #gets lines of text that will be put up with text wraping engaged
-                for x in range(0,len(lines)): #places all lines on screen with text wrapping
-                        text = basicfont.render(lines[x] , True , colorLib[c], (0,0,0))
-                        textRect = text.get_rect()
-                        textRect.centerx = self.win.get_rect().centerx
-                        textRect.centery = sh/3 + h * x # used to determine where to place the letters so that they aren't on top of each other, also starts text 1/3 down screen.
-                        self.win.blit(text , textRect)
-                pygame.display.update() #updates window all at once
-                ##--Comment out the next three lines to prevent text-to-speach--##
-                if c == 'R': os.system('espeak -ven-us+m1 """'+txt+'"""')
-                elif c == 'G': os.system('espeak -ven-us+f3 """'+txt+'"""')
-                elif c == 'O': os.system('espeak -ven-sc+m7 """'+txt+'"""')
-                ##--------------------------------------------------------------##
-        
-        #Display image in window
-        def showImg(self , fName):
-                self.clearWin()
-                if os.path.isfile(fName):
-                        img = pygame.image.load(fName)
-                        w , h = self.size
-                        imgW = h*3/4
-                        self.win.blit(pygame.transform.scale(img, (imgW,h)) , (w/2-imgW/2,0))
-                        pygame.display.flip()
-                else: print 'File not found: ' + fName
- 
-        def truncline(self,text, font, maxwidth):
-                real=len(text)       
-                stext=text           
-                l=font.size(text)[0]
-                cut=0
-                a=0                  
-                done=1
-                old = None
-                while l > maxwidth:
-                        a=a+1
-                        n=text.rsplit(None, a)[0]
-                        if stext == n:
-                                cut += 1
-                                stext= n[:-cut]
-                        else:
-                                stext = n
-                        l=font.size(stext)[0]
-                        real=len(stext)               
-                        done=0
-                        
-                return real, done, stext             
-                
-        def wrapline(self,text, font, maxwidth): 
-                done=0                      
-                wrapped=[]                  
-                                       
-                while not done:             
-                        nl, done, stext=self.truncline(text, font, maxwidth) 
-                        wrapped.append(stext.strip())                  
-                        text=text[nl:]                                 
-                return wrapped        
-         
+	def __init__(self , ht , wt):
+		"""Init window"""
+		pygame.init()
+		self.size = (ht , wt)
+		self.win = screen = pygame.display.set_mode(self.size,HWSURFACE|DOUBLEBUF|RESIZABLE)
 
+	def clearWin(self):
+		"""Clear text or image from screen"""
+		basicfont = pygame.font.SysFont(None , 48)
+		text = basicfont.render("" , True , colorLib['B'] , (0,0,0))
+		textrect = text.get_rect()
+		textrect.centerx = self.win.get_rect().centerx
+		textrect.centery = self.win.get_rect().centery
+		self.win.fill(colorLib['B'])
+		self.win.blit(text , textrect)
+		pygame.display.flip()
 
+	def showText(self , txt,c):
+		"""Display text in window"""
+		resize = 1 #Alter this value to adjust text width relative to window width
+		self.clearWin()
+		self.win.fill(colorLib['B'])
+		basicfont = pygame.font.SysFont(None, 128)
+		sw, sh = self.size
+		w, h = basicfont.size(txt) #gets size of font
+		lines = self.wrapline(txt,basicfont,self.size[1]*resize) #gets lines of text that will be put up with text wraping engaged
+		for x in range(0,len(lines)): #places all lines on screen with text wrapping
+			text = basicfont.render(lines[x] , True , colorLib[c], (0,0,0))
+			textRect = text.get_rect()
+			textRect.centerx = self.win.get_rect().centerx
+			textRect.centery = sh/3 + h * x # used to determine where to place the letters so that they aren't on top of each other, also starts text 1/3 down screen.
+			self.win.blit(text , textRect)
+		pygame.display.update() #updates window all at once
+		##--Comment out the next three lines to prevent text-to-speach--##
+		if c == 'R': os.system('espeak -ven-us+m1 """'+txt+'"""')
+		elif c == 'G': os.system('espeak -ven-us+f3 """'+txt+'"""')
+		elif c == 'O': os.system('espeak -ven-sc+m7 """'+txt+'"""')
+		##--------------------------------------------------------------##
+
+	def showImg(self , fName):
+		"""Display image in window"""
+		self.clearWin()
+		if os.path.isfile(fName):
+			img = pygame.image.load(fName)
+			w , h = self.size
+			imgW = h*3/4
+			self.win.blit(pygame.transform.scale(img, (imgW,h)) , (w/2-imgW/2,0))
+			pygame.display.flip()
+		else: print 'File not found: ' + fName
+
+	def truncline(self,text, font, maxwidth):
+		real=len(text)
+		stext=text
+		l=font.size(text)[0]
+		cut=0
+		a=0
+		done=1
+		old = None
+		while l > maxwidth:
+			a=a+1
+			n=text.rsplit(None, a)[0]
+			if stext == n:
+					cut += 1
+					stext= n[:-cut]
+			else:
+					stext = n
+			l=font.size(stext)[0]
+			real=len(stext)
+			done=0
+		return real, done, stext
+
+	def wrapline(self,text, font, maxwidth):
+		done=0
+		wrapped=[]
+		while not done:
+			nl, done, stext=self.truncline(text, font, maxwidth)
+			wrapped.append(stext.strip())
+			text=text[nl:]
+		return wrapped
 
 def main():
-        #Init socket
-        screenSocket = socket(AF_INET, SOCK_STREAM)
-        screenSocket.bind(('' , port))
-        screenSocket.listen(1)
-        
-        #Create screen
-        disp = screen(1920 , 1080)
-        quitFlag = False
-        while not quitFlag:
-                
-                #Recieve connection / command
-                connectionSocket , addr = screenSocket.accept()
-                connectionSocket.settimeout(defaultTimeout)
-                msg = connectionSocket.recv(1024).strip()
-                
-                #Command indent
-                #Text
-                if msg[0] == 'T': disp.showText(msg[2:].strip(),msg[1].upper())
-                #Image
-                elif msg[0] == 'I': disp.showImg(msg[1:].strip())
-                #Clear
-                elif msg[0] == 'C': disp.clearWin()
-                #Quit
-                elif msg == 'QUIT': quitFlag = True
-                #Catch
-                else: print 'Not a valid command'
-                
-                #Close connection
-                connectionSocket.close()
-        
-        #Close socket
-        screenSocket.close()
+	#Init socket
+	screenSocket = socket(AF_INET, SOCK_STREAM)
+	screenSocket.bind(('' , port))
+	screenSocket.listen(1)
+
+	#Create screen
+	disp = screen(1920 , 1080) #Set window dimentions here
+	quitFlag = False
+	while not quitFlag:
+		#Recieve connection / command
+		connectionSocket , addr = screenSocket.accept()
+		connectionSocket.settimeout(defaultTimeout)
+		msg = connectionSocket.recv(1024).strip()
+		#Command ident
+		#Text
+		if msg[0] == 'T': disp.showText(msg[2:].strip(),msg[1].upper())
+		#Image
+		elif msg[0] == 'I': disp.showImg(msg[1:].strip())
+		#Clear
+		elif msg[0] == 'C': disp.clearWin()
+		#Quit
+		elif msg == 'QUIT': quitFlag = True
+		#Catch
+		else: print 'Not a valid command'
+		#Close connection
+		connectionSocket.close()
+	#Close socket
+	screenSocket.close()
 
 
 if __name__ == '__main__':
         main()
-
-
